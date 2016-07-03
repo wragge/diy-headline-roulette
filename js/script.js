e5ef17d3f559754054e5c440af152a692e086919
Tim Sherratt <tim@discontents.com.au>
2016-07-03 21:41:22 +1000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,248 @@
+
+$(function(){
+
+    // YOU MUST EDIT THIS SECTION
+    // You must supply a Trove API key
+    var troveAPIKey = '';
+    // Either provide full API query here or include options below
+    var apiQuery = '';
+    // Words you want to search for -- separate multiple values with spaces, eg:
+    // var keywords = 'weather wragge';
+    var keywords = '';
+    // How you want to combine keywords -- all, any, or phrase
+    var keywordType = 'all'
+    // Newspaper id numbers -- separate multiple values with spaces, eg:
+    // var titles = '840 35';
+    var titles = '';
+    // Add a byline, eg:
+    var byline = 'Created by <a href="https://timsherratt.org">Tim Sherratt</a>.'
+    // var byline = '';
+    // Add a tagline
+    var tagline = 'How well do you know your Australian history?';
+    // Leave this alone unless you're publishing on a non-https server
+    var useHttps = 'true';
+
+    // YOU CAN ALSO EDIT THESE MESSAGES IF YOU WANT TO
+    // These are the hints you get after each guess
+    // gt100 means the guess was greater than the target year + 100 years etc
+    var messages = {};
+    messages['gt100'] = "Ummm... really... that's your guess? It's much much earlier!";
+    messages['gt50'] = "Oh no! You're way off. It's a lot earlier.";
+    messages['gt10'] = "Getting close, but it's still quite a bit earlier.";
+    messages['gt1'] = "Almost! Try a bit earlier.";
+    messages['lt100'] = "Come on, get serious! It's much much later.";
+    messages['lt50'] = "Nope, missed the mark there. It's a lot later.";
+    messages['lt10'] = "Not bad, but it's still quite a bit later.";
+    messages['lt1'] = "So close! Try a bit later.";
+
+    // These are the messages you get if you guess correctly
+    // The numbers 1-10 represent to number of guesses taken
+    var success_messages = [];
+    success_messages[1] = 'What! How did you... are you cheating?';
+    success_messages[2] = 'The force is strong with this one...';
+    success_messages[3] = 'We salute you and your profound temporal knowledge!';
+    success_messages[4] = 'Excellent work!';
+    success_messages[5] = "Don't get cocky kid...";
+    success_messages[6] = 'A good solid effort. Some room for improvement.';
+    success_messages[7] = 'Not bad. Needs to pay more attention in class.';
+    success_messages[8] = 'You had us worried, but you got there in the end.';
+    success_messages[9] = 'Living dangerously huh? You only just made it.';
+    success_messages[10] = 'Eeek! A last gasp victory!';
+
+    // YOU DON'T REALLY NEED TO EDIT ANYTHING ELSE
+
+    var total = 0;
+
+    function clean_query() {
+        var query;
+        if (useHttps === 'false') {
+            query = 'http://api.trove.nla.gov.au/result?zone=newspaper&l-category=Article&encoding=json'
+        } else {
+            query = 'https://trove-proxy.herokuapp.com/api/result?zone=newspaper&l-category=Article&encoding=json'
+        }
+        if (apiQuery !== "") {
+            query = apiQuery.replace(/&n=\d+/, "").replace(/&s=\d+/, "").replace(/&key=[a-z0-9]+/, "").replace(/&encoding=xml/, "&encoding=xml")
+        } else {
+            if (keywords !== '') {
+                words = keywords.split(' ');
+                console.log(words);
+                if (keywordType == 'any') {
+                    q = words.join('+OR+');
+                } else if (keywordType == 'phrase') {
+                    q = '"' + words.join('+') + '"';
+                } else {
+                    q = words.join('+AND+');
+                }
+            } else {
+                q = '%20';
+            }
+            query += '&q=' + q;
+            if (titles !== '') {
+                $.each(titles.split(' '), function(index, title) {
+                    query += '&l-title=' + title;
+                });
+            }
+        }
+        return query
+    }
+    function get_article_query() {
+        var number = Math.floor(Math.random() * total);
+        var query = clean_query()
+        query = query + "&n=1&s=" + number + "&key=" + troveAPIKey;
+        return query;
+    }
+    function get_random_article() {
+        $("#headline").text('Choosing a random article...');
+        $("#article").showLoading();
+        var query;
+        if (total === 0) {
+            query = clean_query()
+            query = query + "&n=0&key=" + troveAPIKey;
+            get_api_result(query, 'total');
+        } else {
+            query = get_article_query();
+            get_api_result(query, 'article');
+        }
+    }
+    function get_api_result(query, type) {
+        $.ajax({
+            "dataType": "jsonp",
+            "url": query,
+            "success": function(results) {
+                process_results(results, type);
+            },
+            error: function(xmlReq, txtStatus, errThrown){
+                $('#status').text(xmlReq.responseText);
+            }
+        });
+    }
+    function process_results(results, type) {
+        if (type == 'total') {
+            total = results.response.zone[0].records.total;
+            console.log(total);
+            query = get_article_query();
+            get_api_result(query, 'article');
+        } else if (type == 'article') {
+            var article = results.response.zone[0].records.article[0];
+            display_article(article);
+        }
+    }
+    function display_article(article) {
+        var date = $.format.date(article.date + ' 00:00:00.000', 'd MMMM yyyy');
+        var year = article.date.substr(0,4);
+        $('#headline').html(mask_year(article.heading, year));
+        $('#summary').html(mask_year(article.snippet, year));
+        if (article.title.value.indexOf('(')) {
+            newspaper = article.title.value.substr(0, article.title.value.indexOf('(') - 1);
+        } else {
+            newspaper = article.title.value;
+        }
+        $('#paper').html(newspaper);
+        $('#summary, #paper, #count').show();
+        $('#date').text(date);
+        $('#article-link').html('<a class="btn btn-mini btn-primary" href="' + article.troveUrl + '">Read article</a>');
+        $('#year').data('year', year);
+        $("#article").hideLoading();
+        $("#year").focus();
+        console.log(year)
+    }
+    function mask_year(text, year) {
+        text = text.replace(year, '****');
+        return text;
+    }
+    function guess() {
+        var guess = $("#year").val();
+        var guesses = $("#guesses").data("guesses") + 1;
+        if (guess == $("#year").data('year')) {
+            correct(guesses);
+        } else {
+            if (guesses < 10) {
+                give_message(guess, guesses);
+                $("#guesses").data("guesses", guesses);
+                $("#guesses").text(10 - guesses);
+                if (guesses == 4) {
+                    $("#text-guesses").removeClass('status-ok').addClass('status-warning');
+                    $("#count").removeClass('border-ok').addClass('border-warning');
+                } else if (guesses == 7) {
+                    $("#text-guesses").removeClass('status-warning').addClass('status-danger');
+                    $("#count").removeClass('border-warning').addClass('border-danger');
+                }
+                if (guesses == 9) {
+                    $("#text-guesses").text('guess left');
+                }
+                $("#year").focus();
+            } else {
+                $("#text-guesses").text('guesses left');
+                fail();
+            }
+
+        }
+    }
+    function correct(guesses) {
+        $("#status").html("<b>That's it!</b><br>" + success_messages[guesses]).removeClass().addClass('alert alert-success');
+        $("#pub_details").show();
+    }
+    function give_message(guess, guesses) {
+        var year = $("#year").data('year');
+        var difference = parseInt(guess, 10) - year;
+        var message;
+        if (difference >= 100) {
+            message = messages['gt100'];
+            $("#status").removeClass().addClass('alert alert-danger').text(message);
+        } else if (difference < 100 && difference >= 50) {
+            message = messages['gt50'];
+            $("#status").removeClass().addClass('alert alert-warning').text(message);
+        } else if (difference < 50 && difference >= 10) {
+            message = messages['gt10'];
+            $("#status").removeClass().addClass('alert alert-info').text(message);
+        } else if (difference < 10 && difference >= 1) {
+            message = messages['gt1'];
+            $("#status").removeClass().addClass('alert alert-success').text(message);
+        } else if (difference <= -100) {
+            message = messages['lt100'];
+            $("#status").removeClass().addClass('alert alert-danger').text(message);
+        } else if (difference > -100 && difference <= -50) {
+            message = messages['lt50'];
+            $("#status").removeClass().addClass('alert alert-warning').text(message);
+        } else if (difference > -50 && difference <= -10) {
+            message = messages['lt10'];
+            $("#status").removeClass().addClass('alert alert-info').text(message);
+        } else if (difference > -10 && difference <= -1) {
+            message = messages['lt1'];
+            $("#status").removeClass().addClass('alert alert-success').text(message);
+        }
+
+    }
+    function fail() {
+        $("#guesses").text(0);
+        $("#status").removeClass().addClass('alert alert-danger').text("Ooops! Follow the link to read the article, or hit reload to try again.");
+        $("#pub_details").show();
+    }
+    function reset() {
+        $("#year").val("");
+        $("#headline").text('Choosing a random article...');
+        $("#guesses").data("guesses", 0).text(10);
+        $("#text-guesses").removeClass('status-warning status-danger').addClass('status-ok');
+        $("#count").removeClass('border-warning border-danger').addClass('border-ok');
+        $("#summary").empty();
+        $("#paper").empty();
+        $("#pub_details").hide();
+        $("#status").empty().removeClass();
+        get_random_article();
+    }
+    $('#year').keydown(function(event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            guess();
+        }
+    });
+    $("#guess-button").button().click(function() { guess(); });
+    $("#reload-button").button().click(function() { reset(); });
+    $("#guesses").data("guesses", 0);
+    $("#pub_details").hide();
+    $('#tagline').html(tagline);
+    $('#byline').html(byline);
+    get_random_article();
+});
+
+
---
+++ readme.md
@@ -0,0 +1,125 @@
+# DIY Trove Headline Roulette
+
+Headline Roulette is a simple game that challenges you to guess the publication date of a newspaper article chosen at random from Trove.
+
+Here you can build your own customised version of Headline Roulette with nothing more than a GitHub account.
+
+For example, you might to create a version of Headline Roulette limited to articles:
+
+* that mention *cats* or *kittens*
+* that include the phrase "White Australia"
+* that come from the Melbourne **Age**
+* that come from your local region
+
+It's up to you. Build a version for your workshop, class, or community. Build a version just for fun. Any search you can construct in Trove you can use to seed your game. 
+
+## Quickstart
+
+1. Get yourself a [GitHub](https://github.com) account (the free version is fine) and log in.
+2. Get yourself a [Trove API key](http://help.nla.gov.au/trove/building-with-trove/api).
+2. Come back to this page and click on the 'Fork' button (in the top right hand corner of the page) to save a copy of this repository under your own account. [More about forking](https://help.github.com/articles/fork-a-repo/).
+3. Go to your account and view the repository you've just created. It will look just like this one!
+4. Click on the 'Settings' tab and change the repository name to suit your game.
+5. Click on the 'Code' tab. Open the `js` folder and then click on the `script.js` file to view it.
+6. Click on the pencil icon to edit the file.
+7. Look for the **'YOU MUST EDIT THIS SECTION'** message in the `script.js` file and add the details of your game as described in the [customisation section](#customisation-guide) below.
+8. Click on the 'Commit changes' button to save your details.
+9. That's it! Your hew game will be available at the address -- http://**[your Github user name]**.github.io/**[your repository name]**. For example, my user account is 'wragge' and I created a version of this repository called 'forecasters-demo', so you can find it online at http://wragge.github.io/forecasters-demo/ .
+
+## Customisation guide
+
+There are only two things you have to supply:
+
+* your Trove API query
+* your query
+
+In addition, you can set:
+
+* a tagline for your game (appears in the header)
+* a byline for your game (appears in the footer)
+* the messages that players see when they make a guess
+
+There are samples of each of these (except the API key) in the `script.js` file. They all sit inside a block of code labelled 'YOU MUST EDIT THIS SECTION'.
+
+This is the what the main configuration block looks like:
+
+``` javascript
+    // YOU MUST EDIT THIS SECTION
+    // You must supply a Trove API key
+    var troveAPIKey = '';
+    // Either provide full API query here or include options below
+    var apiQuery = '';
+    // Words you want to search for -- separate multiple values with spaces, eg:
+    // var keywords = 'weather wragge';
+    var keywords = '';
+    // How you want to combine keywords -- all, any, or phrase
+    var keywordType = 'all'
+    // Newspaper id numbers -- separate multiple values with spaces, eg:
+    // var titles = '840 35';
+    var titles = '';
+    // Add a byline, eg:
+    var byline = 'Created by <a href="https://timsherratt.org">Tim Sherratt</a>.'
+    // var byline = '';
+    // Add a tagline
+    var tagline = 'How well do you know your Australian history?';
+    // Leave this alone unless you're publishing on a non-https server
+    var useHttps = 'true';
+```
+
+Adding your API key and setting a tagline or byline should be pretty straightforward. Just insert the values you want between the quote marks.
+
+Setting the query your game will use to choose articles is a bit more complicated. You have two main options:
+
+* supply a Trove API query in the form of a url
+* provide keywords and/or the id numbers of particular newspapers
+
+You'll probably only want to supply a Trove API query if you want to use a really complex query -- such as advertisements published in the Argus between 1900 and 1950. For most games you can just use the `keywords` and `titles` settings to build your query automatically.
+
+There are three settings you can use to build your query:
+
+* `keywords` -- the words you want to search for, separated by spaces
+* `keywordType` -- how you want the keywords to be combined, expects one of: `all`, `any`, or `phrase`
+* `titles` -- the id numbers of newspapers you want to search, separated by space
+
+You don't have to use both `keywords` and `titles`, but you can combine them if you want.
+
+The easiest place to get the id numbers of titles is from the [About digitised newspapers](http://trove.nla.gov.au/newspaper/about) page on Trove. If you click on a title, you'll open a new page which has the id of the newspaper at the end of the url. For example the page for the *Canberra Times* is:
+
+
+``` http
+http://trove.nla.gov.au/newspaper/title/11
+
+```
+
+So the id number of the *Canberra Times* is '11'.
+
+Note that if you *do* supply an API query, other settings will be ignored. The easiest way to construct an API query is to use the [Trove API console](https://troveconsole.herokuapp.com/).
+
+## Limitations
+
+Because of the way forking works on GitHub, you can only fork a repository once into your own account. That means you can only create one game.
+
+The easiest, but kludgiest, way around this is to create a new GitHub organisation, and then fork your second game into your new organisation's account. To create a new organisation just click on the '+' sign at the top of the screen and choose 'New organisation'.
+
+If you're making lots of games you probably want to install Git on your local computer and follow the instructions below.
+
+## Managing multiple games
+
+As noted above, you can only create one game using GitHub by itself. Yes, you can a new organisation for every game, but that's going to get messy quickly. Fortunately it's easy to set things up on your local computer so that you can create as many games as you want!
+
+The first step is to [install Git on your local computer](https://git-scm.com/book/en/v2/Getting-Started-Installing-Git).
+
+Once that's done, just follow the following steps each time you want to create a new game:
+
+1. You'll need to [use the command line](http://lifehacker.com/5633909/who-needs-a-mouse-learn-to-use-the-command-line-for-almost-anything), so fire up a terminal.
+2. On the command line type `git clone https://github.com/wragge/diy-headline-roulette.git mynewgame` -- replacing 'mynewgame' with whatever you want. This command will create a directory called `mynewgame` and copy all the files from the DIY-Headline-Roulette repository on GitHub into it.
+3. Type `cd mynewgame` to move to the new directory.
+4. Now you have to create a new repository in your own GitHub account. Just login to your account, click on the '+' button at the top of the page, and choose 'New repository'. Give your repository a name (probably the same as the directory you just created) and click the green 'Create repository' button.
+4. Go back to your terminal and type `git remote rename origin upstream` -- this changes the name of the link to the original repository.
+5. Now you need the address of your new repository. On your new repository's page you should see a url that starts with `https://github.com/` and ends in `.git` -- copy it.
+6. In the terminal type `git remote add origin [your repository's url]` -- inserting the url you just copied. This will link the repository on your local machine with your new repository on GitHub.
+7. Type `git push -u origin gh-pages` -- this copies the repository on your local machine to GitHub.
+
+Now you have a choice, you can edit the files as described above in your new GitHub repository. Or you can use the text editor of your choice to modify the files on your local computer. If you edit them locally, you'll need to copy your modified files to GitHub to see your exhibition. At the terminal just type `git push origin gh-pages` -- easy!
+
+
